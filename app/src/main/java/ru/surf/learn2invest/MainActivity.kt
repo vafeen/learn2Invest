d8afefe022ae994e44943c53c133dfa8ec84473a
Nadezhda <nadyachery@gmail.com>
2024-05-02 21:55:08 +0300

    Revert MainActivity

--- app/src/main/java/ru/surf/learn2invest/MainActivity.kt
+++ app/src/main/java/ru/surf/learn2invest/MainActivity.kt
@@ -10,7 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
