802d479daad6200dd4215cb55950f5888b2371d5
Vafeen <666av6@gmail.com>
2024-05-08 22:45:47 +0300

    Fix bug with package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "ru.surf.surf"
+        applicationId = "ru.surf.learn2invest"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
