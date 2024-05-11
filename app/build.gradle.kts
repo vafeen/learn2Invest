02cf2f71e57900a57040ddc0cb565f6ae9133cb1
Vafeen <666av6@gmail.com>
2024-05-11 18:57:15 +0300

    Integrate notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
