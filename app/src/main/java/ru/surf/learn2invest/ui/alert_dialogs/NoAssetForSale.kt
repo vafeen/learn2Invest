39fb46bb2e60a3e6bbef3ad590206b87e0cc4fa4
Vafeen <666av6@gmail.com>
2024-05-14 06:05:44 +0300

    Add NoAssetForSale dialog && rewrite no_asset_for_sale_dialog.xml

---
+++ app/src/main/java/ru/surf/learn2invest/ui/alert_dialogs/NoAssetForSale.kt
@@ -0,0 +1,31 @@
+package ru.surf.learn2invest.ui.alert_dialogs
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import ru.surf.learn2invest.databinding.NoAssetForSaleDialogBinding
+import ru.surf.learn2invest.ui.alert_dialogs.parent.CustomAlertDialog
+
+class NoAssetForSale(
+    context: Context,
+    layoutInflater: LayoutInflater
+) : CustomAlertDialog(context = context) {
+
+    private var binding =
+        NoAssetForSaleDialogBinding.inflate(layoutInflater)
+
+    override fun setCancelable(): Boolean {
+        return true
+    }
+
+    override fun initListeners() {
+        binding.buttonExitRefillAccountDialog.setOnClickListener {
+            cancel()
+        }
+    }
+
+    override fun getDialogView(): View {
+        return binding.root
+    }
+
+}
