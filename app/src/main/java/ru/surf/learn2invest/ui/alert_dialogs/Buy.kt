a329c1a1f8b835d0a5f29a589b4b36a7a6fc33de
Vafeen <666av6@gmail.com>
2024-05-26 05:34:01 +0300

    Add logic Buy dialog && fix buy_dialog.xml

--- app/src/main/java/ru/surf/learn2invest/ui/alert_dialogs/Buy.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/alert_dialogs/Buy.kt
@@ -1,14 +1,17 @@
 package ru.surf.learn2invest.ui.alert_dialogs
 
 import android.content.Context
+import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import androidx.lifecycle.LifecycleCoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.surf.learn2invest.databinding.BuyDialogBinding
 import ru.surf.learn2invest.main.Learn2InvestApp
+import ru.surf.learn2invest.noui.logs.Loher
 import ru.surf.learn2invest.ui.alert_dialogs.parent.CustomAlertDialog
 
 class Buy(
@@ -24,14 +27,20 @@ class Buy(
     }
 
     override fun initListeners() {
+
+
         binding.apply {
-            lifecycleScope.launch(Dispatchers.IO) {
-                val profileList = Learn2InvestApp.mainDB.profileDao().getProfile()
-                balanceTextviewBuyDialog.text = if (profileList.isNotEmpty()) {
-                    profileList[0].fiatBalance.toString()
-                    // TODO(Володь, то, что выше, нужно исправить, т.к. я хз, что тут за баланс)
-                } else {
-                    "error balance"
+          balanceNumBuyDialog.text =
+                Learn2InvestApp.profile?.fiatBalance.toString() // TODO()Володь, Сюда также нужно 
+            //            поставить нужный тип баланса
+
+            lifecycleScope.launch(Dispatchers.Main) {
+                while (true) {
+                    priceNumberBuyDialog.setText("777777") // TODO Сюда нужно будет кидать цену,
+                    // которая приходит через ретрофит
+
+                    updateFields()
+                    delay(2000)
                 }
             }
 
@@ -40,23 +49,97 @@ class Buy(
             }
 
             buttonBuyBuyDialog.setOnClickListener {
+                // TODO Логика продажи
+
                 cancel()
             }
-//            editText1.addTextChangedListener(object : TextWatcher {
-//                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//
-//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                    editText2.setText(s) // Устанавливаем текст из первого поля во второе
-//                }
-//
-//                override fun afterTextChanged(s: Editable?) {}
-//            })
+
+            imageButtonPlusBuyDialog.setOnClickListener {
+                enteringNumberOfLotsBuyDialog.setText(enteringNumberOfLotsBuyDialog.text.let { text ->
+
+                    val newNumberOfLots = if (text.isNotEmpty()) {
+                        text.toString().toIntOrNull()?.let {
+                            val balance = Learn2InvestApp.profile?.fiatBalance
+                                ?: 0 // TODO(Володь, тут также поменять баланс на нужный)
+                            val itog = itog(onFuture = true)
+                            Loher.d("itog = $itog")
+                            if (itog(onFuture = true) <= balance) {
+                                it + 1
+                            } else {
+                                it
+                            }
+                        } ?: 0
+                    } else {
+                        1
+                    }
+
+                    "$newNumberOfLots"
+                })
+            }
+
+            imageButtonMinusBuyDialog.setOnClickListener {
+                enteringNumberOfLotsBuyDialog.setText(enteringNumberOfLotsBuyDialog.text.let { text ->
+                    val newNumberOfLots = text.toString().toIntOrNull()?.let {
+                        if (it > 0) {
+                            it - 1
+                        } else {
+                            it
+                        }
+                    } ?: 0
+
+                    "$newNumberOfLots"
+                })
+            }
+
+            enteringNumberOfLotsBuyDialog.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    updateFields()
+                }
+            })
 
         }
     }
 
     override fun getDialogView(): View {
         return binding.root
+    }
+
+    private fun updateFields() {
+        binding.itogoBuyDialog.text = "Итого: $ ${itog(onFuture = false)}"
+    }
+
+   
+    private fun itog(
+        onFuture: Boolean
+    ): Float {
+        binding.apply {
+            val priceText = priceNumberBuyDialog.text.toString()
+
+            val price = priceText.substring(1, priceText.length)
+                .toFloatOrNull() ?: 0f
+
+            val number = enteringNumberOfLotsBuyDialog.text.toString()
+                .toIntOrNull() ?: 0
+
+            return price * (number + if (onFuture) {
+                1
+            } else {
+                0
+            })
+        }
     }
 
 }
