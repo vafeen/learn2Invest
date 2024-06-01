f9b5db560f10cce7904e124d319fcd69476fbf75
Nadezhda <nadyachery@gmail.com>
2024-06-01 11:48:50 +0300

    Remove RequiresAPI annotation

--- app/src/main/java/ru/surf/learn2invest/chart/LineChartStyle.kt
+++ app/src/main/java/ru/surf/learn2invest/chart/LineChartStyle.kt
@@ -2,6 +2,7 @@ package ru.surf.learn2invest.chart
 
 import android.content.Context
 import android.graphics.Color
+import android.icu.util.Calendar
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.github.mikephil.charting.charts.LineChart
@@ -9,13 +10,14 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
 import ru.surf.learn2invest.R
+import java.text.SimpleDateFormat
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class LineChartStyle(private val context: Context) {
     companion object {
-        @RequiresApi(Build.VERSION_CODES.O)
-        private val timeFormatter = DateTimeFormatter.ofPattern("H:mm")
+        private val timeFormatter = SimpleDateFormat("H:mm", Locale.getDefault())
     }
 
     fun styleChart(lineChart: LineChart) = lineChart.apply {
@@ -64,10 +66,12 @@ class LineChartStyle(private val context: Context) {
     }
 
     private class TimeValueFormatter : ValueFormatter() {
-        @RequiresApi(Build.VERSION_CODES.O)
+        //@RequiresApi(Build.VERSION_CODES.O)
         override fun getFormattedValue(value: Float): String {
-            val localTime = LocalTime.of(value.toInt(), 0)
-            return timeFormatter.format(localTime)
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, value.toInt())
+            calendar.set(Calendar.MINUTE,0)
+            return timeFormatter.format(calendar.time)
         }
     }
 }
