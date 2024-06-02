5ee8720df98ebf30760e51bc10ddc84d7f2734ac
Vafeen <666av6@gmail.com>
2024-06-03 01:21:26 +0300

    Add Profile flow

--- app/src/main/java/ru/surf/learn2invest/app/App.kt
+++ app/src/main/java/ru/surf/learn2invest/app/App.kt
@@ -1,6 +1,8 @@
 package ru.surf.learn2invest.app
 
 import android.app.Application
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
 import ru.surf.learn2invest.notifications.NotificationChannels
 import ru.surf.learn2invest.notifications.registerNotificationChannels
 import ru.surf.learn2invest.noui.database_components.L2IDatabase
@@ -13,13 +15,15 @@ class App : Application() {
 
         var idOfProfile = 0
 
-        var profile: Profile? = null
+        lateinit var profile: Flow<List<Profile>>
     }
 
     override fun onCreate() {
         super.onCreate()
 
         mainDB = L2IDatabase.buildDatabase(context = this)
+
+        profile = mainDB.profileDao().getAllAsFlow()
 
         this.registerNotificationChannels(NotificationChannels.allChannels)
 
