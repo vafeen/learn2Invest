500934e8a9b502d82c75e15d2f5bf56f5d5fdc6e
Vladimir <worobev2001@gmail.com>
2024-06-03 16:50:18 +0300

    Add new request and rework old

--- app/src/main/java/ru/surf/learn2invest/network_components/CoinAPIService.kt
+++ app/src/main/java/ru/surf/learn2invest/network_components/CoinAPIService.kt
@@ -3,14 +3,17 @@ package ru.surf.learn2invest.network_components
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.surf.learn2invest.network_components.responses.APIWrapper
+import ru.surf.learn2invest.network_components.responses.AugmentedCoinReviewResponse
 import ru.surf.learn2invest.network_components.util.Const.API_HISTORY
 import ru.surf.learn2invest.network_components.util.Const.API_MARKET_REVIEW
-import ru.surf.learn2invest.network_components.responses.CoinHistoryResponse
-import ru.surf.learn2invest.network_components.responses.MarketReviewResponse
+import ru.surf.learn2invest.network_components.responses.CoinPriceResponse
+import ru.surf.learn2invest.network_components.responses.CoinReviewResponse
+import ru.surf.learn2invest.network_components.util.Const.API_COIN_REVIEW
 
 interface CoinAPIService {
     @GET(API_MARKET_REVIEW)
-    suspend fun getMarketReview(): MarketReviewResponse
+    suspend fun getMarketReview(): APIWrapper<List<CoinReviewResponse>>
 
     @GET(API_HISTORY)
     suspend fun getCoinHistory(
@@ -18,5 +21,10 @@ interface CoinAPIService {
         @Query("interval") interval: String,
         @Query("start") start: Long,
         @Query("end") end: Long
-    ): CoinHistoryResponse
+    ): APIWrapper<List<CoinPriceResponse>>
+
+    @GET(API_COIN_REVIEW)
+    suspend fun getCoinReview(
+        @Path("id") id: String
+    ): APIWrapper<AugmentedCoinReviewResponse>
 }
--- app/src/main/java/ru/surf/learn2invest/network_components/util/Const.kt
+++ app/src/main/java/ru/surf/learn2invest/network_components/util/Const.kt
@@ -4,6 +4,7 @@ object Const {
     const val BASE_URL = "https://api.coincap.io/v2/"
     const val API_MARKET_REVIEW = "${BASE_URL}assets?limit=2000"
     const val API_HISTORY = "${BASE_URL}assets/{id}/history"
+    const val API_COIN_REVIEW = "${BASE_URL}assets/{id}"
     const val WEEK: Long = 604800000 // миллисекунды
-    const val INTERVAL: String = "h12" // интервал выборки данных. Доступные форматы m1, m5, m15, m30, h1, h2, h6, h12, d1
+    const val INTERVAL: String = "d1" // интервал выборки данных. Доступные форматы m1, m5, m15, m30, h1, h2, h6, h12, d1
 }
