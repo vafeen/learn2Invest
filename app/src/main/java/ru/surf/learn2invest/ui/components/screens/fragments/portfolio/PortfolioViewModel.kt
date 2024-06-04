3069e1f9edfe8edbc95ebd17a3acdd96465801ef
Nadezhda <nadyachery@gmail.com>
2024-06-04 11:01:21 +0300

    Add test data for portfolio

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/portfolio/PortfolioViewModel.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/portfolio/PortfolioViewModel.kt
@@ -3,24 +3,43 @@ package ru.surf.learn2invest.ui.components.screens.fragments.portfolio
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.mikephil.charting.data.Entry
+import kotlinx.coroutines.launch
 import ru.surf.learn2invest.noui.database_components.dao.AssetBalanceHistoryDao
+import ru.surf.learn2invest.noui.database_components.entity.AssetBalanceHistory
 
-class PortfolioViewModel : ViewModel() {
+class PortfolioViewModel(
+    private val assetBalanceHistoryDao: AssetBalanceHistoryDao
+) : ViewModel() {
     private val _chartData = MutableLiveData<List<Entry>>()
     val chartData: LiveData<List<Entry>> get() = _chartData
 
+    val testData = listOf(
+        AssetBalanceHistory(assetBalance = 5f),
+        AssetBalanceHistory(assetBalance = 4f),
+        AssetBalanceHistory(assetBalance = 7f),
+        AssetBalanceHistory(assetBalance = 8f),
+        AssetBalanceHistory(assetBalance = 10f),
+        AssetBalanceHistory(assetBalance = 7f),
+        AssetBalanceHistory(assetBalance = 3f)
+    )
+
     fun loadChartData() {
-        // Здесь загружаются данные из базы данных или сервера
-        val data: List<Entry> = listOf(
-            Entry(0f, 5f),
-            Entry(1f, 4f),
-            Entry(2f, 7f),
-            Entry(3f, 8f),
-            Entry(4f, 10f),
-            Entry(5f, 7f),
-            Entry(6f, 3f)
-        )
-        _chartData.value = data
+        viewModelScope.launch {
+            assetBalanceHistoryDao.getAllAsFlow().collect{
+                balanceHistories ->
+                val data = balanceHistories.mapIndexed { index, assetBalanceHistory ->
+                    Entry(index.toFloat(), assetBalanceHistory.assetBalance)
+                }
+                _chartData.value = data
+            }
+        }
+    }
+
+    fun insertTestData() {
+        viewModelScope.launch {
+            assetBalanceHistoryDao.insertAll(*testData.toTypedArray())
+        }
     }
 }
