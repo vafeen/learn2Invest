9ba2b4d5e1b21abfe8957bc51b84ef066769557f
Nadezhda <nadyachery@gmail.com>
2024-06-06 00:25:24 +0300

    Add sign up button logic

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/SignUpActivity.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/SignUpActivity.kt
@@ -1,12 +1,25 @@
 package ru.surf.learn2invest.ui.components.screens
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.surf.learn2invest.R
+import ru.surf.learn2invest.app.App
 import ru.surf.learn2invest.databinding.ActivitySignupBinding
+import ru.surf.learn2invest.noui.cryptography.PasswordHasher
+import ru.surf.learn2invest.noui.database_components.entity.Profile
+import ru.surf.learn2invest.ui.components.screens.sign_in.SignINActivityActions
+import ru.surf.learn2invest.ui.components.screens.sign_in.SignInActivity
+import ru.surf.learn2invest.ui.tests.data.testProfile
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
@@ -25,6 +38,37 @@ class SignUpActivity : AppCompatActivity() {
 
         setupNameClearIconImageView()
         setupLastnameClearIconImageView()
+
+        binding.inputNameEditText.addTextChangedListener {
+            validateFields()
+        }
+
+        binding.inputLastnameEditText.addTextChangedListener {
+            validateFields()
+        }
+
+        binding.signupBtn.setOnClickListener {
+            val profile = Profile(
+                id = 0,
+                firstName = name,
+                lastName = lastname,
+                pin = 0,
+                notification = false,
+                biometry = false,
+                confirmDeal = false,
+                fiatBalance = 0,
+                assetBalance = 0,
+            )
+            lifecycleScope.launch(Dispatchers.IO) {
+                App.mainDB.profileDao().insertAll(profile)
+            }
+            startActivity(Intent(this@SignUpActivity, SignInActivity::class.java).let {
+                it.action = SignINActivityActions.SignUP.action
+
+                it
+            })
+            this@SignUpActivity.finish()
+        }
     }
 
     private fun setupNameClearIconImageView() {
@@ -71,6 +115,16 @@ class SignUpActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {}
         }
         binding.inputLastnameEditText.addTextChangedListener(textWatcher)
+    }
+
+    private fun validateFields() {
+        if (name.isNotEmpty() && lastname.isNotEmpty()) {
+            binding.signupBtn.isEnabled = true
+            binding.signupBtn.backgroundTintList = ContextCompat.getColorStateList(this, R.color.main_background)
+        } else {
+            binding.signupBtn.isEnabled = false
+            binding.signupBtn.backgroundTintList = ContextCompat.getColorStateList(this, R.color.btn_asset)
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
