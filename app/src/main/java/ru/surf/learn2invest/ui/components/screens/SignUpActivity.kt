f026d7c8b8e516ba140083edf3c53d655c7f333b
Nadezhda <nadyachery@gmail.com>
2024-06-06 17:09:58 +0300

    Remove unused imports

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/SignUpActivity.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/SignUpActivity.kt
@@ -6,8 +6,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
