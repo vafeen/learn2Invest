99ef8588d38b4f11d4bafab411d67971b60cf3bd
Vladimir <worobev2001@gmail.com>
2024-06-08 19:47:42 +0300

    Binding fix

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/subhistory/SubHistoryFragment.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/subhistory/SubHistoryFragment.kt
@@ -1,5 +1,9 @@
 package ru.surf.learn2invest.ui.components.screens.fragments.subhistory
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,11 +14,21 @@ import ru.surf.learn2invest.app.App
 import ru.surf.learn2invest.databinding.FragmentAssetHistoryBinding
 import ru.surf.learn2invest.noui.database_components.entity.Transaction
 
-class SubHistoryFragment: Fragment() {
-    private val binding by lazy { FragmentAssetHistoryBinding.inflate(layoutInflater) }
+class SubHistoryFragment : Fragment() {
+    private lateinit var binding: FragmentAssetHistoryBinding
     private val data = mutableListOf<Transaction>()
     private var symbol: String? = null
     private val adapter = SubHistoryAdapter(data)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAssetHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onStart() {
         super.onStart()
         symbol = arguments?.getString("SYMBOL") //TODO Определиться где будут все константы
@@ -22,7 +36,7 @@ class SubHistoryFragment: Fragment() {
         binding.assetHistory.adapter = adapter
         if (symbol.isNullOrBlank().not())
             lifecycleScope.launch(Dispatchers.IO) {
-                App.mainDB.transactionDao().getFilteredBySymbol(symbol!!).collect{
+                App.mainDB.transactionDao().getFilteredBySymbol(symbol!!).collect {
                     data.addAll(it)
                     withContext(Dispatchers.Main) {
                         adapter.notifyDataSetChanged()
