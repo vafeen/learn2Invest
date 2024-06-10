cdc9b32ccc7023e06f5cbc4311dfe8100d2520fb
Vafeen <666av6@gmail.com>
2024-06-11 02:28:58 +0300

    Add default drawer

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/portfolio/PortfolioFragment.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/portfolio/PortfolioFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.navigation.NavigationView
+import ru.surf.learn2invest.R
 import ru.surf.learn2invest.app.App
 import ru.surf.learn2invest.chart.LineChartHelper
 import ru.surf.learn2invest.databinding.FragmentPortfolioBinding
@@ -33,6 +38,55 @@ class PortfolioFragment : Fragment() {
         }
         viewModel.loadChartData()
 
+        initDrawer()
+
+        binding.imageButton.setOnClickListener {
+            activity?.apply {
+                val drawer = binding.drawerLayout
+
+                if (!drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.openDrawer(GravityCompat.START)
+                }
+            }
+
+        }
+
         return binding.root
+    }
+
+    private fun initDrawer() {
+        activity?.apply {
+            val drawerLayout: DrawerLayout = binding.drawerLayout
+            val navView: NavigationView = binding.navView
+
+            val toggle = ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+            )
+
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            // Handle navigation item clicks
+            navView.setNavigationItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.nav_home -> {
+                        // Handle Home click
+                        // Replace with your logic
+                        false
+                    }
+
+                    R.id.nav_profile -> {
+                        // Handle Profile click
+                        // Replace with your logic
+                        false
+                    }
+                    // Add more cases for other menu items
+                    else -> true
+                }
+            }
+        }
     }
 }
