e0065b11567c2d6484eba2d04814257664eee082
Vafeen <666av6@gmail.com>
2024-06-12 22:08:44 +0300

    Rebase main -> features/fix_sign_in_activity

--- app/src/main/java/ru/surf/learn2invest/ui/main/MainActivity.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,7 +16,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.surf.learn2invest.R
 import ru.surf.learn2invest.app.App
+import ru.surf.learn2invest.app.App.Companion.mainDB
+import ru.surf.learn2invest.app.App.Companion.profile
 import ru.surf.learn2invest.databinding.ActivityMainBinding
+import ru.surf.learn2invest.noui.cryptography.PasswordHasher
+import ru.surf.learn2invest.noui.database_components.entity.Profile
 import ru.surf.learn2invest.ui.components.screens.SignUpActivity
 import ru.surf.learn2invest.ui.components.screens.sign_in.SignINActivityActions
 import ru.surf.learn2invest.ui.components.screens.sign_in.SignInActivity
@@ -49,30 +54,78 @@ class MainActivity : AppCompatActivity() {
     // Функция проверки, есть ли у нас зарегистрированный пользователь
     private fun skipSplash() {
         lifecycleScope.launch(Dispatchers.IO) {
+            mainDB.profileDao().getAllAsFlow().collect { profList ->
+                if (profList.isNotEmpty()) {
+                    profile = profList[App.idOfProfile]
+                    Log.d("profile", "profile APP no else = $profile ")
 
-            val undef = "undefined"
+                    val undef = "undefined"
 
-            val intent = if (App.profile.firstName == undef &&
-                App.profile.lastName == undef
-            ) {
-                delay(1500)
-                Intent(this@MainActivity, SignUpActivity::class.java)
-            } else {
-                withContext(Dispatchers.Main) {
-                    runAnimatedText()
-                    delay(2000)
+                    val intent = if (App.profile.firstName == undef &&
+                        App.profile.lastName == undef
+                    ) {
+                        delay(1500)
+                        Intent(this@MainActivity, SignUpActivity::class.java)
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            runAnimatedText()
+                            delay(2000)
+                        }
+
+                        Intent(this@MainActivity, SignInActivity::class.java).let {
+                            it.action = SignINActivityActions.SignIN.action
+
+                            it
+                        }
+                    }
+                } else {
+                    profile = Profile(
+                        id = 0,
+                        firstName = "undefined",
+                        lastName = "undefined",
+                        biometry = false,
+                        fiatBalance = 50540f,
+                        assetBalance = 0f,
+                        hash = PasswordHasher(
+                            firstName = "undefined",
+                            lastName = "undefined"
+                        ).passwordToHash("0000"),
+                        tradingPasswordHash = PasswordHasher(
+                            firstName = "undefined",
+                            lastName = "undefined"
+                        ).passwordToHash("1235789")
+                    )
+
+                    mainDB.profileDao().insertAll(profile)
+
+                    Log.d("profile", "profile APP else  = $profile ")
+
                 }
-                Intent(this@MainActivity, SignInActivity::class.java).let {
-                    it.action = SignINActivityActions.SignIN.action
 
-                    it
+                delay(1000)
+
+                val undef = "undefined"
+
+                val intent = if (profile.firstName == undef &&
+                    profile.lastName == undef
+                ) {
+
+                    Intent(this@MainActivity, SignUpActivity::class.java)
+
+                } else {
+
+                    Intent(this@MainActivity, SignInActivity::class.java).let {
+                        it.action = SignINActivityActions.SignIN.action
+
+                        it
+                    }
+
                 }
 
+                startActivity(intent)
+
+                this@MainActivity.finish()
             }
-
-            startActivity(intent)
-
-            this@MainActivity.finish()
         }
     }
 
