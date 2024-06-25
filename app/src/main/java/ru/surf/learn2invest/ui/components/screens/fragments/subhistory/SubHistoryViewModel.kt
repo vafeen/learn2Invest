efd12c1660b55f3cbfe8cbb67ce103de08634384
Vladimir <worobev2001@gmail.com>
2024-06-25 22:22:04 +0300

    Add transaction history revert

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/subhistory/SubHistoryViewModel.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/fragments/subhistory/SubHistoryViewModel.kt
@@ -2,9 +2,10 @@ package ru.surf.learn2invest.ui.components.screens.fragments.subhistory
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import ru.surf.learn2invest.noui.database_components.DatabaseRepository
 import ru.surf.learn2invest.noui.database_components.entity.Transaction.Transaction
 
 class SubHistoryViewModel(val symbol: String): ViewModel() {
-    val data: Flow<List<Transaction>> = DatabaseRepository.getFilteredBySymbolTransaction(symbol)
+    val data: Flow<List<Transaction>> = DatabaseRepository.getFilteredBySymbolTransaction(symbol).map { it.reversed() }
 }
