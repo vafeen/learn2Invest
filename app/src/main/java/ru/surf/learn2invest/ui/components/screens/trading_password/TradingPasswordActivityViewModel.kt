f1d4092b722d9ff6d1abd9e343ac899464538d43
Vafeen <666av6@gmail.com>
2024-06-28 14:16:28 +0300

    Add DI to all activities

--- app/src/main/java/ru/surf/learn2invest/ui/components/screens/trading_password/TradingPasswordActivityViewModel.kt
+++ app/src/main/java/ru/surf/learn2invest/ui/components/screens/trading_password/TradingPasswordActivityViewModel.kt
@@ -3,17 +3,25 @@ package ru.surf.learn2invest.ui.components.screens.trading_password
 import android.content.Context
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.surf.learn2invest.R
 import ru.surf.learn2invest.noui.cryptography.PasswordHasher
 import ru.surf.learn2invest.noui.database_components.DatabaseRepository
+import javax.inject.Inject
 
-class TradingPasswordActivityViewModel : ViewModel() {
+@HiltViewModel
+class TradingPasswordActivityViewModel @Inject constructor() : ViewModel() {
     lateinit var action: TradingPasswordActivityActions
     lateinit var actionName: String
     lateinit var mainButtonAction: String
 
+    @Inject
+    lateinit var databaseRepository: DatabaseRepository
     fun saveTradingPassword(password: String) {
-        DatabaseRepository.apply {
+        databaseRepository.apply {
             profile = if (action == TradingPasswordActivityActions.CreateTradingPassword ||
                 action == TradingPasswordActivityActions.ChangeTradingPassword
             ) {
@@ -28,6 +36,11 @@ class TradingPasswordActivityViewModel : ViewModel() {
             } else profile.copy(tradingPasswordHash = null)
         }
     }
+
+    fun updateProfile() =
+        viewModelScope.launch(Dispatchers.IO) {
+            databaseRepository.updateProfile(databaseRepository.profile)
+        }
 
     fun initAction(intentAction: String, context: Context): Boolean {
         var isOk = true
